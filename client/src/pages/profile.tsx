2cabcfc4511ff65645d93bbba3961481f055cc33
VaibhavPatil06 <vaibhavvpatill@gmail.com>
2026-03-17 13:06:30 +0530

    added url in profile

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -141,14 +141,14 @@ export default function Profile() {
                                         className="text-blue-600 cursor-pointer flex items-center justify-center  gap-2"
                                         onClick={() => {
                                             navigator.clipboard.writeText(
-                                               "https://deod.ai/register?ref=" + user.wallet_address,
+                                               "https://front.deod.ai/register?ref=" + user.wallet_address,
                                             );
                                             alert("Copied to clipboard");
                                         }}
                                         title="Copy to clipboard"
                                     >
                                         <span className="text-xs">
-                                            {"https://deod.ai/register?ref=" + user.wallet_address}
+                                            {"https://front.deod.ai/register?ref=" + user.wallet_address}
                                         </span>{" "}
                                         <Copy className="w-4 h-4" />
                                     </a>
